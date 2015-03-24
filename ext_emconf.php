254e3bc180a62ff3832490001014c9d0fa68b6d9
Claus Due <claus@namelesscoder.net>
2015-03-24 13:04:07 +0100

    [TASK] Initialise extension resources
    Summary:
    This change adds extension metadata files and class
    registrations for integrating a new FAL driver.
    
    Reviewers: #forgetit-dev, dkd-egerer
    
    Reviewed By: #forgetit-dev, dkd-egerer
    
    Differential Revision: https://phabricator.dkd.de/D1075

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "cmis_fal".
+ *
+ * Auto generated 19-03-2015 10:09
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'CMIS FAL Driver',
+	'description' => 'Driver to use a CMIS Repository as FAL storage',
+	'category' => 'misc',
+	'author' => 'ForgetIT',
+	'author_email' => '',
+	'author_company' => '',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'experimental',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.11-7.1.99',
+			'cms' => ''
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:0:{}',
+);
+
