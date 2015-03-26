edcd91e0994c134921cff3d3144697949a6cee92
Sascha Egerer <sascha.egerer@dkd.de>
2015-03-26 18:49:12 +0100

    [BUGFIX] Fixed typo in classname CmisFilesystemDriver in ext_localconf
    Summary:
    The ext_localconf.php registeres the FAL driver with the class name
    `\Dkd\CmisFal\Driver\CmisFileSystemDriver`. But the class and file name is
    called `CmisFilesystemDriver` (lowercase `s`) which fails on case sensitive
    systems.
    
    Reviewers: claus.due, #forgetit-dev
    
    Reviewed By: claus.due, #forgetit-dev
    
    Differential Revision: https://phabricator.dkd.de/D1111

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['registeredDrivers']['cmis'] = array(
-	'class' => 'Dkd\\CmisFal\\Driver\\CMISFileSystemDriver',
+	'class' => 'Dkd\\CmisFal\\Driver\\CMISFilesystemDriver',
 	'label' => 'CMIS',
 	'flexFormDS' => 'FILE:EXT:cmis_fal/Configuration/CMISDriverFlexForm.xml'
 );
