a39a8279d2ab5682dcd129952d94b320d6b5132c
Claus Due <claus@namelesscoder.net>
2017-03-15 15:40:22 +0100

    [BUGFIX] Refactor to restore 6.2 support
    Technically, the extension does support 6.2 with older versions of cmis_service.
    
    The FAL driver change happened in 7.6, not v8 - refactored accordingly.

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-8.7.99'
+			'typo3' => '6.2.11-8.7.99'
 		),
 		'conflicts' => array(
 		),
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,10 +3,10 @@ defined('TYPO3_MODE') or die('Access denied');
 
 (function() {
 
-    $driverClassName = \Dkd\CmisFal\Driver\Versioned\V7\CMISFilesystemDriver::class;
+    $driverClassName = \Dkd\CmisFal\Driver\Versioned\V6\CMISFilesystemDriver::class;
 
-    if (version_compare(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getExtensionVersion('core'), 8, '>=')) {
-        $driverClassName = \Dkd\CmisFal\Driver\Versioned\V8\CMISFilesystemDriver::class;
+    if (version_compare(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getExtensionVersion('core'), "7.6", '>=')) {
+        $driverClassName = \Dkd\CmisFal\Driver\Versioned\V7\CMISFilesystemDriver::class;
     }
 
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['registeredDrivers']['cmis'] = array(
